7f0b50b59c826ff8b2aca11a27a8b365cfd6b452
jichani <jichan7571@naver.com>
2023-07-17 16:41:02 +0900

    edit

--- src/routers/rentalRouter.js
+++ src/routers/rentalRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { rentalNotice, rentalNoticeWrite } from "../controllers/rentalControllers.js";
+import { rentalNotice, rentalNoticeDetail, rentalNoticeWrite } from "../controllers/rentalControllers.js";
 
 const rentalRouter = express.Router();
 
 rentalRouter.get("/notice", rentalNotice);
 rentalRouter.post("/notice-write", rentalNoticeWrite);
+rentalRouter.get("/notice-detail/:id", rentalNoticeDetail);
 
 export default rentalRouter;
