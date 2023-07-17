6bbf624493a3cd14cf7ebb78eff7947fde2d85c9
jichani <jichan7571@naver.com>
2023-07-17 15:56:01 +0900

    error handle

--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ import "./db.js";
 import express from "express";
 import morgan from "morgan";
 import rentalRouter from "./routers/rentalRouter.js";
+import cors from "cors";
 
 const PORT = 8080;
 const app = express()
 
+const corsOptions = {
+  origin: [
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
 // 라우팅 하기 전에 미들웨어를 해야만 실행이 된다. 현재 자리가 되게 중요함!
 app.use(morgan("dev"));
 app.use(express.json());
