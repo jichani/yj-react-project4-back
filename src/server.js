1b86b13f624bb9be7872ea2fddb9b8c6442e6f9b
jichani <jichan7571@naver.com>
2023-07-17 13:38:32 +0900

    add

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
+import "./db.js";
 import express from "express";
 import morgan from "morgan";
+import rentalRouter from "./routers/rentalRouter.js";
 
 const PORT = 8080;
 const app = express()
 
 // 라우팅 하기 전에 미들웨어를 해야만 실행이 된다. 현재 자리가 되게 중요함!
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/test', function (req, res) {
-  res.send("안녕하세요");
-})
+app.use("/api/rental", rentalRouter);
 
 const handleListening = () => console.log(`😊 server listening on port http://localhost:${PORT} 😊`);
 
