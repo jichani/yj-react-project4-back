82e3847f3a9549c22fbbf5dd99b01f9f8241d3ac
jichani <jichan7571@naver.com>
2023-08-07 13:21:40 +0900

    클라우드 타입에 우리 넷틀리파이 주소를 승인해달라고 보냄

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 const corsOptions = {
   origin: [
-    "http://localhost:3000"
+    "http://localhost:3000", "https://polite-sundae-84c7bd.netlify.app"
   ],
   methods: ["GET", "POST"],
   credentials: true,
