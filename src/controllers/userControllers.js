4400b47d504b4d439a95c1f7f8a54c21ae6c209d
jichani <jichan7571@naver.com>
2023-08-14 13:55:30 +0900

    add

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 // 회원가입
 export const postRegisterMember = async (req, res) => {
@@ -22,7 +23,26 @@ export const postRegisterMember = async (req, res) => {
 }
 
 // 로그인 컨트롤러
-export const postUsernameSignIn = (req, res) => {
-  const body = req.body;
-  console.log(body);
+export const postUsernameSignIn = async (req, res) => {
+  const { username, password } = req.body;
+  // 에러처리
+  if (username === "" || password === "") {
+    res.json({
+      ok: "false",
+      message: "아이디와 패스워드는 반드시 입력해야 합니다."
+    });
+
+  }
+
+  // 아이디 확인
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(401).json({ ok: "false", message: "해당하는 유저가 없습니다." })
+  }
+
+  const ok = await bcrypt.compare(password, user.password)
+  console.log(ok);
+  if (!ok) {
+    return res.status(401).json({ ok: "false", message: "아이디/패스워드가 다릅니다." })
+  }
 }
